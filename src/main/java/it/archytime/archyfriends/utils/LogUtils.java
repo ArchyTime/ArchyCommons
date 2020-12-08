5a8d99c2e87be15790004eac65c1033a207409b2
MattSays <mattsays@archytime.it>
2020-12-08 13:21:42 +0100

    Changed LogUtils from static to an actual object

--- src/main/java/it/archytime/archyfriends/utils/LogUtils.java
+++ src/main/java/it/archytime/archyfriends/utils/LogUtils.java
@@ -4,21 +4,21 @@ import org.slf4j.Logger;
 
 public class LogUtils {
 
-    private static Logger logger;
+    private Logger logger;
 
-    public static void setLogger(Logger logger) {
-        LogUtils.logger = logger;
+    public void setLogger(Logger logger) {
+        this.logger = logger;
     }
 
-    public static void info(String msg) {
+    public void info(String msg) {
         logger.info(msg);
     }
 
-    public static void warn(String msg) {
+    public void warn(String msg) {
         logger.warn(msg);
     }
 
-    public static void error(String msg) {
+    public void error(String msg) {
         logger.error(msg);
     }
 
